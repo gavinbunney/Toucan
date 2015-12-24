fb3cdd76adeee5c59ead3b9a08b0aa2f1cc21eb3
phimage <eric.marchand.n7@gmail.com>
2015-12-24 10:33:07 +0100

    Add TVOS target. Add tvos into podspec. Fix #25

---
+++ ToucanTVOS/ToucanTVOS.h
@@ -0,0 +1,19 @@
+//
+//  ToucanTVOS.h
+//  ToucanTVOS
+//
+//  Created by phimage on 24/12/15.
+//  Copyright © 2015 Bunney Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ToucanTVOS.
+FOUNDATION_EXPORT double ToucanTVOSVersionNumber;
+
+//! Project version string for ToucanTVOS.
+FOUNDATION_EXPORT const unsigned char ToucanTVOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToucanTVOS/PublicHeader.h>
+
+
